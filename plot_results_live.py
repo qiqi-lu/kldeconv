066413ddc4b6e9fab49f85628873206224b55d5b
Qiqi Lu <94900158+qiqi-lu@users.noreply.github.com>
2025-08-05 00:37:46 +0800

    clear results and rename utils.dataset_utils

--- plot_results_live.py
+++ plot_results_live.py
@@ -4,46 +4,49 @@ import numpy as np
 import os
 import utils.image_plot as utils_plot
 import matplotlib as mpl
-import utils.dataset_utils as utils_data
+import utils.data as utils_data
 
 # ------------------------------------------------------------------------------
-dataset_name = 'zeroshotdeconvnet'
+dataset_name = "zeroshotdeconvnet"
 id_sample = [0, 346, 609, 700, 770, 901]
 # id_sample = [7, 8 , 9]
-interval = 10 # s
+interval = 10  # s
 # ------------------------------------------------------------------------------
 # methods = ['raw', 'traditional', 'gaussian', 'butterworth',\
 #            'wiener_butterworth','kernelnet']
 # methods = ['raw', 'wiener_butterworth', 'kernelnet_ss', 'kernelnet']
-methods = ['raw', 'kernelnet']
+methods = ["raw", "kernelnet"]
 
-path_specimen = os.path.join('outputs', 'figures', dataset_name, 'Mitosis')
+path_specimen = os.path.join("outputs", "figures", dataset_name, "Mitosis")
 
-path_fig_data = [os.path.join(path_specimen, '642'),\
-                 os.path.join(path_specimen, '560')]
-path_root = 'F:\\Datasets\\ZeroShotDeconvNet\\'
-path_dataset =\
-    [path_root+'3D time-lapsing data_LLSM_Mitosis_H2B\\642',\
-     path_root+'3D time-lapsing data_LLSM_Mitosis_Mito\\560']
+path_fig_data = [os.path.join(path_specimen, "642"), os.path.join(path_specimen, "560")]
+path_root = "F:\\Datasets\\ZeroShotDeconvNet\\"
+path_dataset = [
+    path_root + "3D time-lapsing data_LLSM_Mitosis_H2B\\642",
+    path_root + "3D time-lapsing data_LLSM_Mitosis_Mito\\560",
+]
 
-with open(os.path.join(path_dataset[0], 'raw.txt')) as f:
+with open(os.path.join(path_dataset[0], "raw.txt")) as f:
     raw_txt = f.read().splitlines()
 
 # ------------------------------------------------------------------------------
 # load results
-print('-'*80)
-print('load result from :', path_fig_data)
+print("-" * 80)
+print("load result from :", path_fig_data)
 img_deconv_mc = []
 
 for id_channel in range(len(path_fig_data)):
     img_deconv = []
     for i in id_sample:
-        path_fig_sample = os.path.join(path_fig_data[id_channel], f'sample_{i}')
-        print('load', path_fig_sample)
+        path_fig_sample = os.path.join(path_fig_data[id_channel], f"sample_{i}")
+        print("load", path_fig_sample)
         imgs = []
         # raw
-        imgs.append(io.imread(os.path.join(path_dataset[id_channel],\
-            methods[0], raw_txt[i])).astype(np.uint16))
+        imgs.append(
+            io.imread(
+                os.path.join(path_dataset[id_channel], methods[0], raw_txt[i])
+            ).astype(np.uint16)
+        )
         # # conventional methods
         # for meth in methods[1:-2]:
         #     imgs.append(io.imread(os.path.join(path_fig_sample, meth,\
@@ -54,81 +57,87 @@ for id_channel in range(len(path_fig_data)):
         # KLD
         # imgs.append(io.imread(os.path.join(path_fig_sample, methods[-1],\
         #     'y_pred_all.tif'))[-1])
-        imgs.append(io.imread(os.path.join(path_fig_sample, methods[-1],\
-            'y_pred_all.tif')))
+        imgs.append(
+            io.imread(os.path.join(path_fig_sample, methods[-1], "y_pred_all.tif"))
+        )
         img_deconv.append(imgs)
     img_deconv_mc.append(img_deconv)
 img_deconv_mc = np.array(img_deconv_mc)
 
 Nc, Nt, Nmeth, Nz, Ny, Nx = img_deconv_mc.shape
-print('Num of channel: {}, Num of time point: {}, num of methods: {},\
-    image shape: {}'.format(Nc, Nt, Nmeth, (Nz, Ny, Nx)))
+print(
+    "Num of channel: {}, Num of time point: {}, num of methods: {},\
+    image shape: {}".format(
+        Nc, Nt, Nmeth, (Nz, Ny, Nx)
+    )
+)
 
 # ------------------------------------------------------------------------------
 # show image restored (merged)
 # ------------------------------------------------------------------------------
 # define color
 nr, nc = 2, Nt
-fig, axes = plt.subplots(nrows=nr, ncols=nc, dpi=300, figsize=(3*nc, 3*nr),\
-    constrained_layout=True)
-for ax in axes.ravel(): ax.set_axis_off()
+fig, axes = plt.subplots(
+    nrows=nr, ncols=nc, dpi=300, figsize=(3 * nc, 3 * nr), constrained_layout=True
+)
+for ax in axes.ravel():
+    ax.set_axis_off()
 
-center_xy, range_xy = Nz//2, 10
-center_xz, range_xz = Ny//2, 10
+center_xy, range_xy = Nz // 2, 10
+center_xz, range_xz = Ny // 2, 10
 
 for imeth in range(Nmeth):
-    print('plot', methods[imeth])
+    print("plot", methods[imeth])
     for it in range(Nt):
         img = img_deconv_mc[:, it, imeth]
         img = np.transpose(img, axes=(1, 2, 3, 0))
 
         # xy plane
         # ----------------------------------------------------------------------
-        xy_plane = np.max(\
-            img[center_xy - range_xy : center_xy + range_xy], axis=0)
+        xy_plane = np.max(img[center_xy - range_xy : center_xy + range_xy], axis=0)
 
         # ----------------------------------------------------------------------
         # xz plane
-        xz_plane = np.max(\
-            img[:, (center_xz - range_xz) : (center_xz + range_xz), :], axis=1)
+        xz_plane = np.max(
+            img[:, (center_xz - range_xz) : (center_xz + range_xz), :], axis=1
+        )
 
         tmp = []
         for i in range(Nc):
-            tmp.append(utils_data.interp(xz_plane[..., i],\
-                ps_xy=92.6, ps_z=200))
+            tmp.append(utils_data.interp(xz_plane[..., i], ps_xy=92.6, ps_z=200))
         xz_plane = np.transpose(np.array(tmp), axes=(1, 2, 0))
 
         # ----------------------------------------------------------------------
-        cmaps = [mpl.colormaps['gray'], mpl.colormaps['afmhot']]
+        cmaps = [mpl.colormaps["gray"], mpl.colormaps["afmhot"]]
 
         if imeth == 0:
             # vmin = [np.percentile(img[..., 0], 3),\
             #         np.percentile(img[..., 1], 3)]
             vmin = [0, 0]
-            vmax = [np.percentile(img[..., 0], 99.99),\
-                    np.percentile(img[..., 1], 99.9)]
+            vmax = [np.percentile(img[..., 0], 99.99), np.percentile(img[..., 1], 99.9)]
         else:
             # vmin = [np.percentile(img[..., 0], 3),\
             #         np.percentile(img[..., 1], 3)]
             vmin = [0, 0]
-            vmax = [np.percentile(xy_plane[..., 0], 99.5),\
-                    np.percentile(xy_plane[..., 1], 99.5)]
-        
+            vmax = [
+                np.percentile(xy_plane[..., 0], 99.5),
+                np.percentile(xy_plane[..., 1], 99.5),
+            ]
+
         # vmin = [0, 0]
         # vmax = [np.percentile(xy_plane[..., 0], 99.5),\
         #         np.percentile(xy_plane[..., 1], 99.5)]
 
-        dict_img = {'cmaps': cmaps, 'vmin':vmin, 'vmax': vmax}
+        dict_img = {"cmaps": cmaps, "vmin": vmin, "vmax": vmax}
         axes[0, it].imshow(utils_plot.render(xy_plane, **dict_img))
-        axes[0, it].text(10, 20,\
-            '{:>.1f} min'.format(interval*id_sample[it]/60), color='white')
+        axes[0, it].text(
+            10, 20, "{:>.1f} min".format(interval * id_sample[it] / 60), color="white"
+        )
         axes[1, it].imshow(utils_plot.render(xz_plane, **dict_img))
 
-    plt.savefig(os.path.join(path_specimen,\
-        f'image_restored_{methods[imeth]}.png'))
-    plt.rcParams['svg.fonttype'] = 'none'
-    plt.savefig(os.path.join(path_specimen,\
-        f'image_restored_{methods[imeth]}.svg'))
+    plt.savefig(os.path.join(path_specimen, f"image_restored_{methods[imeth]}.png"))
+    plt.rcParams["svg.fonttype"] = "none"
+    plt.savefig(os.path.join(path_specimen, f"image_restored_{methods[imeth]}.svg"))
 
 
 # ------------------------------------------------------------------------------
--- plot_results_real_3d.py
+++ plot_results_real_3d.py
@@ -7,76 +7,92 @@ from utils import evaluation as eva
 from matplotlib.gridspec import GridSpec
 import matplotlib.patches as patches
 import matplotlib.colors as colors
-import utils.dataset_utils as utils_data
+import utils.data as utils_data
+
 
 # ------------------------------------------------------------------------------
 def cal_ssim(x, y):
     # need 3D input
-    if y.shape[0] >= 7: # the size of the filter in SSIM is at least 7
-        return eva.SSIM(img_true=y, img_test=x, data_range=y.max() - y.min(),\
-                    multichannel=False, channle_axis=None, version_wang=False)
+    if y.shape[0] >= 7:  # the size of the filter in SSIM is at least 7
+        return eva.SSIM(
+            img_true=y,
+            img_test=x,
+            data_range=y.max() - y.min(),
+            multichannel=False,
+            channle_axis=None,
+            version_wang=False,
+        )
     else:
-        return eva.SSIM(img_true=y, img_test=x, data_range=y.max() - y.min(),\
-                    multichannel=True, channle_axis=0, version_wang=False)
+        return eva.SSIM(
+            img_true=y,
+            img_test=x,
+            data_range=y.max() - y.min(),
+            multichannel=True,
+            channle_axis=0,
+            version_wang=False,
+        )
+
 
 def cal_psnr(x, y):
     # need 3D input
     return eva.PSNR(img_true=y, img_test=x, data_range=y.max() - y.min())
 
+
 # ------------------------------------------------------------------------------
 # data_set_name_test, id_data = 'Microtubule', 0
 # data_set_name_test, id_data = 'Nuclear_Pore_complex', 0
 
-data_set_name_test, id_data = 'Microtubule2', 0
+data_set_name_test, id_data = "Microtubule2", 0
 # data_set_name_test, id_data = 'Nuclear_Pore_complex2', 0
 
 # ------------------------------------------------------------------------------
-name_net = 'kernelnet'
+name_net = "kernelnet"
 eps = 0.000001
 
-fig_path_data = os.path.join('outputs', 'figures', data_set_name_test.lower(),\
-    f'scale_1_gauss_0_poiss_0_ratio_1')
-fig_path_sample = os.path.join(fig_path_data, f'sample_{id_data}')
+fig_path_data = os.path.join(
+    "outputs", "figures", data_set_name_test.lower(), f"scale_1_gauss_0_poiss_0_ratio_1"
+)
+fig_path_sample = os.path.join(fig_path_data, f"sample_{id_data}")
 
-if data_set_name_test in ['Microtubule', 'Nuclear_Pore_complex']:
+if data_set_name_test in ["Microtubule", "Nuclear_Pore_complex"]:
     fig_path_ker = fig_path_data
 
-if data_set_name_test in ['Microtubule2', 'Nuclear_Pore_complex2']:
-    fig_path_ker = os.path.join(fig_path_data, 'kernels_bc_1_re_1')
+if data_set_name_test in ["Microtubule2", "Nuclear_Pore_complex2"]:
+    fig_path_ker = os.path.join(fig_path_data, "kernels_bc_1_re_1")
 
 # ------------------------------------------------------------------------------
 # load results
-print('load results from :', fig_path_sample)
-print('load kernels from :', fig_path_ker)
+print("load results from :", fig_path_sample)
+print("load kernels from :", fig_path_ker)
 
-ker_init = io.imread(os.path.join(fig_path_ker, 'kernel_init.tif'))
-ker_true = io.imread(os.path.join(fig_path_ker, 'kernel_true.tif'))
-ker_FP   = io.imread(os.path.join(fig_path_ker, 'kernel_fp.tif'))
-ker_BP   = io.imread(os.path.join(fig_path_ker, 'kernel_bp.tif'))
+ker_init = io.imread(os.path.join(fig_path_ker, "kernel_init.tif"))
+ker_true = io.imread(os.path.join(fig_path_ker, "kernel_true.tif"))
+ker_FP = io.imread(os.path.join(fig_path_ker, "kernel_fp.tif"))
+ker_BP = io.imread(os.path.join(fig_path_ker, "kernel_bp.tif"))
 
 if ker_init.shape[-1] == 3:
     ker_init = np.transpose(ker_init, axes=(-1, 0, 1))
     ker_true = np.transpose(ker_true, axes=(-1, 0, 1))
-    ker_FP   = np.transpose(ker_FP, axes=(-1, 0, 1))
-    ker_BP   = np.transpose(ker_BP, axes=(-1, 0, 1))
+    ker_FP = np.transpose(ker_FP, axes=(-1, 0, 1))
+    ker_BP = np.transpose(ker_BP, axes=(-1, 0, 1))
 
-y      = io.imread(os.path.join(fig_path_sample, name_net, 'y.tif'))
-x      = io.imread(os.path.join(fig_path_sample, name_net, 'x.tif'))
-x0     = io.imread(os.path.join(fig_path_sample, name_net, 'x0.tif'))
-y_fp   = io.imread(os.path.join(fig_path_sample, name_net, 'y_fp.tif'))
-x0_fp  = io.imread(os.path.join(fig_path_sample, name_net, 'x0_fp.tif'))
-bp     = io.imread(os.path.join(fig_path_sample, name_net, 'bp.tif'))
-y_pred = io.imread(os.path.join(fig_path_sample, name_net, 'y_pred_all.tif'))
+y = io.imread(os.path.join(fig_path_sample, name_net, "y.tif"))
+x = io.imread(os.path.join(fig_path_sample, name_net, "x.tif"))
+x0 = io.imread(os.path.join(fig_path_sample, name_net, "x0.tif"))
+y_fp = io.imread(os.path.join(fig_path_sample, name_net, "y_fp.tif"))
+x0_fp = io.imread(os.path.join(fig_path_sample, name_net, "x0_fp.tif"))
+bp = io.imread(os.path.join(fig_path_sample, name_net, "bp.tif"))
+y_pred = io.imread(os.path.join(fig_path_sample, name_net, "y_pred_all.tif"))
 y_pred = y_pred[2]
 
 Nz_t, Ny_t, Nx_t = ker_true.shape
 Nz_f, Ny_f, Nx_f = ker_FP.shape
 Nz_b, Ny_b, Nx_b = ker_BP.shape
-Nz, Ny, Nx       = y.shape
+Nz, Ny, Nx = y.shape
 
 # ------------------------------------------------------------------------------
-vmax_psf, color_map_psf = 0.01, 'hot'
-vmax_img, color_map_img = y.max() * 0.6, 'gray'
+vmax_psf, color_map_psf = 0.01, "hot"
+vmax_img, color_map_img = y.max() * 0.6, "gray"
 vmax_diff = vmax_img
 
 # # ------------------------------------------------------------------------------
@@ -96,7 +112,7 @@ vmax_diff = vmax_img
 
 # axes[0,2].plot(ker_true[Nz_t//2, :, Nx_t//2], color='black', label='True')
 # axes[0,2].plot(ker_init[Nz_t//2, :, Nx_t//2], color='blue', label='init')
-# axes[0,2].plot(ker_FP[Nz_f//2, :, Nx_f//2], color='red', label=name_net) 
+# axes[0,2].plot(ker_FP[Nz_f//2, :, Nx_f//2], color='red', label=name_net)
 
 # axes[1,2].plot(ker_true[:, Ny_t//2, Nx_t//2], color='black', label='True')
 # axes[1,2].plot(ker_init[:, Ny_t//2, Nx_t//2], color='blue', label='init')
@@ -178,7 +194,7 @@ vmax_diff = vmax_img
 # # ------------------------------------------------------------------------------
 # # show FFT of the forward kernel
 # # ------------------------------------------------------------------------------
-# ker_true_fft = utils_data.fft_n(ker_true, s=y.shape) 
+# ker_true_fft = utils_data.fft_n(ker_true, s=y.shape)
 # ker_FP_fft   = utils_data.fft_n(ker_FP, s=y.shape)
 # S_fft        = ker_FP_fft.shape
 # vmax_fft, color_map_fft = ker_true_fft.max(), 'hot'
@@ -194,7 +210,7 @@ vmax_diff = vmax_img
 # axes[0,1].set_title('FT(PSF (learned))')
 
 # axes[0,0].imshow(ker_true_fft[S_fft[0]//2], **dict_ker_fft)
-# axes[0,1].imshow(ker_FP_fft[S_fft[0]//2], **dict_ker_fft)   
+# axes[0,1].imshow(ker_FP_fft[S_fft[0]//2], **dict_ker_fft)
 # axes[1,0].imshow(ker_true_fft[:, S_fft[1]//2, :], **dict_ker_fft)
 # axes[1,1].imshow(ker_FP_fft[:, S_fft[1]//2, :], **dict_ker_fft)
 
@@ -262,70 +278,72 @@ vmax_diff = vmax_img
 # axes[2,4].set_title('BP(LR/FP(x0))')
 
 # axes[0,5].imshow(y_pred[Nz//2], **dict_img)
-# axes[1,5].imshow(np.abs(y_pred-y)[Nz//2], **dict_img_diff)   
+# axes[1,5].imshow(np.abs(y_pred-y)[Nz//2], **dict_img_diff)
 # axes[2,5].imshow(y_pred[:, Ny//2, :], **dict_img)
 # axes[3,5].imshow(np.abs(y_pred-y)[:, Ny//2, :], **dict_img_diff)
 
 # axes[0,5].set_title('xk ({:.4f})'.format(cal_ssim(y_pred, y)))
 # axes[1,5].set_title('|xk-HR| ({:.2f})'.format(cal_psnr(y_pred, y)))
 # axes[2,5].set_title('xk')
-# axes[3,5].set_title('|xk-HR|') 
+# axes[3,5].set_title('|xk-HR|')
 
 # plt.savefig(os.path.join(fig_path_sample, 'img_bp_inter'))
 
 # ------------------------------------------------------------------------------
 # show image restored
 # ------------------------------------------------------------------------------
-print('-'*80)
-print('plot restored images ...')
+print("-" * 80)
+print("plot restored images ...")
 data = []
 data.append(x)
-data.append(io.imread(os.path.join(fig_path_sample, 'deconvblind',\
-    'deconv.tif')))
-data.append(io.imread(os.path.join(fig_path_sample, 'traditional',\
-    'deconv_20.tif')))
+data.append(io.imread(os.path.join(fig_path_sample, "deconvblind", "deconv.tif")))
+data.append(io.imread(os.path.join(fig_path_sample, "traditional", "deconv_20.tif")))
 data.append(y_pred)
 data.append(y)
 data = np.array(data)
 
-N_meth = data.shape[0] # number of methods (include raw and gt)
+N_meth = data.shape[0]  # number of methods (include raw and gt)
 
 # ------------------------------------------------------------------------------
-print('-'*80)
-print('PSNR | SSIM:')
-for i in range(N_meth-1):
+print("-" * 80)
+print("PSNR | SSIM:")
+for i in range(N_meth - 1):
     print(cal_psnr(data[i], data[-1]), cal_ssim(data[i], data[-1]))
-print('-'*80)
+print("-" * 80)
 
 # ------------------------------------------------------------------------------
-if data_set_name_test in ['Nuclear_Pore_complex', 'Nuclear_Pore_complex2']: 
-    pos, size = [200,300], [150,300] # 0
-    cmap = colors.LinearSegmentedColormap.from_list("", \
-        ["black","#21D416","white"]) # green
-    color_map_img, color_box_edge = cmap, 'white'
+if data_set_name_test in ["Nuclear_Pore_complex", "Nuclear_Pore_complex2"]:
+    pos, size = [200, 300], [150, 300]  # 0
+    cmap = colors.LinearSegmentedColormap.from_list(
+        "", ["black", "#21D416", "white"]
+    )  # green
+    color_map_img, color_box_edge = cmap, "white"
 
-if data_set_name_test in ['Microtubule', 'Microtubule2']:
-    pos, size = [200, 200], [150,300] # 0
-    cmap = colors.LinearSegmentedColormap.from_list("", \
-        ["black","#03AED2","white"]) # blue
-    color_map_img, color_box_edge = cmap, 'white'
+if data_set_name_test in ["Microtubule", "Microtubule2"]:
+    pos, size = [200, 200], [150, 300]  # 0
+    cmap = colors.LinearSegmentedColormap.from_list(
+        "", ["black", "#03AED2", "white"]
+    )  # blue
+    color_map_img, color_box_edge = cmap, "white"
 
-dict_img = {'cmap':color_map_img, 'vmin':0.0, 'vmax':data[-1].max()*0.6}
+dict_img = {"cmap": color_map_img, "vmin": 0.0, "vmax": data[-1].max() * 0.6}
+
 
 # ------------------------------------------------------------------------------
 def interp(x):
     ps_xy, ps_z = 25, 160
-    z_scale = ps_z/ps_xy
+    z_scale = ps_z / ps_xy
     x = torch.tensor(x)[None, None]
-    x = torch.nn.functional.interpolate(x, scale_factor=(z_scale, 1, 1),\
-        mode='nearest')
-    x = x.numpy()[0,0]
+    x = torch.nn.functional.interpolate(x, scale_factor=(z_scale, 1, 1), mode="nearest")
+    x = x.numpy()[0, 0]
     return x
+
 
 # ------------------------------------------------------------------------------
 nr, nc = 3, 5
-fig, axes = plt.subplots(nrows=nr, ncols=nc, dpi=300, figsize=(3*nc, 3*nr),\
-    constrained_layout=True)
+fig, axes = plt.subplots(
+    nrows=nr, ncols=nc, dpi=300, figsize=(3 * nc, 3 * nr), constrained_layout=True
+)
 
 for ax in axes.ravel():
     ax.set_axis_off()
@@ -334,19 +352,24 @@ id_slice = 1
 for i in range(N_meth):
     # whole slice
     img = data[i][id_slice]
-    axes[0,i].imshow(img, **dict_img)
-    box = patches.Rectangle(xy=(pos[1], pos[0]), width=size[1], height=size[0],\
-        fill=False, edgecolor=color_box_edge)
-    axes[0,i].add_patch(box)
+    axes[0, i].imshow(img, **dict_img)
+    box = patches.Rectangle(
+        xy=(pos[1], pos[0]),
+        width=size[1],
+        height=size[0],
+        fill=False,
+        edgecolor=color_box_edge,
+    )
+    axes[0, i].add_patch(box)
     # pacth
-    patch = img[pos[0]:pos[0]+size[0], pos[1]:pos[1]+size[1]]
-    axes[1,i].imshow(patch, **dict_img)
+    patch = img[pos[0] : pos[0] + size[0], pos[1] : pos[1] + size[1]]
+    axes[1, i].imshow(patch, **dict_img)
     # xz plane of pacth
     img_inter = interp(data[i])
-    patch_inter = img_inter[:, pos[0]:pos[0]+size[0], pos[1]:pos[1]+size[1]]
+    patch_inter = img_inter[:, pos[0] : pos[0] + size[0], pos[1] : pos[1] + size[1]]
     Nz_patch, Ny_patch, Nx_patch = patch_inter.shape
-    axes[2,i].imshow(patch_inter[:, Ny_patch//2, :], **dict_img)
+    axes[2, i].imshow(patch_inter[:, Ny_patch // 2, :], **dict_img)
 
-plt.savefig(os.path.join(fig_path_sample, 'image_restored.png'))
-plt.rcParams['svg.fonttype'] = 'none'
-plt.savefig(os.path.join(fig_path_sample, 'image_restored.svg'))
+plt.savefig(os.path.join(fig_path_sample, "image_restored.png"))
+plt.rcParams["svg.fonttype"] = "none"
+plt.savefig(os.path.join(fig_path_sample, "image_restored.svg"))
